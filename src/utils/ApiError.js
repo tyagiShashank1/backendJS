3a6ea5fd916d9c414d3999854a47788f84620a84
Shashank Tyagi <shashank.s.tyagi@gmail.com>
2024-05-26 17:27:23 +0530

    stack spelling

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -4,7 +4,7 @@ class ApiError extends Error{
     statusCode,
     message="Something went wrong",
     errors=[],
-    statck=""
+    stack=""
   ){
 
     super(message)
@@ -16,8 +16,8 @@ class ApiError extends Error{
 
 
 
-    if(statck){
-      this.statck=statck
+    if(stack){
+      this.stack=stack
     }
     else{
       Error.captureStackTrace(this,this.constructor)
